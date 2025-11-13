c1044dcb2b26bb58d4e26929b7f0418da0de1ddb
ishtar <secretgoddess6666@gmail.com>
2025-11-13 15:08:08 +0100

    task 1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,3 +17,46 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName}. ${lastName}`;
+}
+
+console.log(printTeacher('John', 'Doe'));
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student1 = new StudentClass('Jane', 'Smith');
+console.log(student1.displayName());  // Output: Jane
+console.log(student1.workOnHomework());  // Output: Currently working
